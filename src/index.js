245c047e846aae978cf708b7c13627bbecbc8042
Kawin Glomjai <g.kawin@live.com>
2018-08-31 23:01:26 +0700

    simple wrap modal

--- src/index.js
+++ src/index.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class ModalDestination extends React.PureComponent {
+class WrapDestination extends React.PureComponent {
   constructor (props) {
     super(props)
     this.node = document.createElement('div')
+    this.node.id = 'wrap-destination'
     document.body.appendChild(this.node)
   }
 
-  componentWillUnmount () {
-    if (this.node) {
-      document.body.removeChild(this.node)
-    }
-    this.node = null
+  render () {
+    return null
   }
+}
+
+class Modal extends React.PureComponent {
   render () {
     return ReactDOM.createPortal(
       this.props.children,
-      this.node
+      document.getElementById('wrap-destination')
     )
   }
 }
 
 const App = () => (
   <div>
-    <ModalDestination />
+    <WrapDestination />
     <h1>
         THis is level 1
       <div>
         <h2>this is level 2</h2>
-
+        <Modal>
+          fuckingC
+        </Modal>
       </div>
     </h1>
   </div>
