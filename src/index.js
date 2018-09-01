1d22bb68aca6709527077838da5b6c6fe07cc756
Kawin Glomjai <g.kawin@live.com>
2018-09-01 12:41:52 +0700

    [WIP]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { connect, Provider } from 'react-redux'
+
+import { createStore, combineReducers } from 'redux'
+
+const createActions = (subType = {}) => {
+  let type = {}
+  Object.keys(subType).forEach((name) => {
+    const schema = subType[name]
+    type[name] = (payload) => Object.assign({ type: name, payload: schema })
+  })
+  return type
+}
+
+const Actions = createActions({
+  openedModal: { isOpen: true },
+  closedModal: { isOpen: false }
+})
+
+const reducers = combineReducers({
+  modal: (state = { }, payload) => {
+    switch (payload.type) {
+      case 'openedModal': {
+        return Object.assign({}, state.modal, payload.payload)
+      }
+    }
+    return state
+  }
+})
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class WrapDestination extends React.PureComponent {
   constructor (props) {
@@ -24,18 +57,30 @@ class Modal extends React.PureComponent {
 }
 
 const App = () => (
-  <div>
-    <WrapDestination />
-    <h1>
+  <Provider store={store}>
+    <React.Fragment>
+      <WrapDestination />
+      <h1>
         THis is level 1
-      <div>
-        <h2>this is level 2</h2>
-        <Modal>
-          fuckingC
-        </Modal>
-      </div>
-    </h1>
-  </div>
+        <ClickMe />
+      </h1>
+    </React.Fragment>
+  </Provider>
 )
+
+const ClickMe = connect((state, ownProps) => {
+  return ({ isOpen: state.modal.isOpen })
+})(({ isOpen }) => {
+  return (<div>
+    <h2>this is level 2</h2>
+    <Modal>
+      <button onClick={() => store.dispatch(Actions.closedModal())}>Close</button>
+      <button onClick={() => store.dispatch(Actions.openedModal())}>Open</button>
+      <div>{
+        isOpen ? 'foo' : 'bar'
+      }</div>
+    </Modal>
+  </div>)
+})
 
 export default ReactDOM.render(<App />, document.getElementById('render'))
